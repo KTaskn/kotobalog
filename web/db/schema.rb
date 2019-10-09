cd26992c3dca5fb5e67850b2f24410b9886614c6
KTaskn <korai.kinjo@gmail.com>
2019-10-09 22:15:31 +0900

    add sentence model

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_124033) do
+ActiveRecord::Schema.define(version: 2019_10_09_131347) do
 
   create_table "access_tokens", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -39,6 +39,17 @@ ActiveRecord::Schema.define(version: 2019_10_09_124033) do
     t.index ["user_id"], name: "index_refresh_tokens_on_user_id"
   end
 
+  create_table "sentences", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "book_id"
+    t.bigint "user_id"
+    t.string "sentence"
+    t.datetime "registration_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_sentences_on_book_id"
+    t.index ["user_id"], name: "index_sentences_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
@@ -49,4 +60,6 @@ ActiveRecord::Schema.define(version: 2019_10_09_124033) do
 
   add_foreign_key "access_tokens", "users"
   add_foreign_key "refresh_tokens", "users"
+  add_foreign_key "sentences", "books"
+  add_foreign_key "sentences", "users"
 end
