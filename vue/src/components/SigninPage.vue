f0ce8bb43848592e2736520759943517cd0f9524
KTaskn <korai.kinjo@gmail.com>
2019-10-13 09:06:55 +0900

    signin機能を追加

--- vue/src/components/SigninPage.vue
+++ vue/src/components/SigninPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <b-form @submit="onSubmit">
+      <span v-show=haserror>ログインできませんでした</span>
       <b-form-group id="input-group-1" class="text-left" label="Name:" label-for="input-1">
         <b-form-input
           id="input-1"
@@ -27,20 +28,51 @@
 </template>
 
 <script>
+import Global from '@/global/index'
+
 export default {
   components: {
   },
   data () {
     return {
       form: {
-        name: '',
-        password: ''
-      }
+        name: 'name',
+        password: 'test_password'
+      },
+      haserror: false
     }
   },
   methods: {
     onSubmit (evt) {
+      this.post_signin('/user/signin', this.form)
       return ''
+    },
+    post_signin (url, data) {
+      return Global.post_wrapper(
+        url,
+        {
+          name: data.name,
+          password: data.password
+        }
+      ).then((res) => {
+        if (res.data.result) {
+          localStorage.name = res.data.name
+          localStorage.access_token = res.data.access_token
+          localStorage.access_token_expiration = res.data.access_token_expiration
+          localStorage.refresh_token = res.data.refresh_token
+          localStorage.refresh_token_expiration = res.data.refresh_token_expiration
+          this.$router.push({ path: '/note' })
+          this.$eventHub.$emit('raise_show_signout')
+        } else {
+          localStorage.removeItem('name')
+          localStorage.removeItem('access_token')
+          localStorage.removeItem('refresh_token')
+          localStorage.removeItem('access_token_expiration')
+          localStorage.removeItem('refresh_token_expiration')
+          this.$eventHub.$emit('drop_show_signout')
+          this.haserror = true
+        }
+      })
     }
   }
 }
