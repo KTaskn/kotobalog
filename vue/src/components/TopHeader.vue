09afc401328bad113e8e1360056110ad7eb32dee
KTaskn <korai.kinjo@gmail.com>
2019-10-20 22:28:18 +0900

    ヘッダーの文章を変更

--- vue/src/components/TopHeader.vue
+++ vue/src/components/TopHeader.vue
@@ -3,7 +3,7 @@
     <b-row id="topheader-text" align-v="center">
       <b-col cols="12">
         <h1 class="text-left">コトバをためる</h1>
-        <p class="text-left">コトバノートは小説やポエムの一節、または偉人の名言、街角の看板、いろいろなところに書かれたコトバを記録するためのサービスです。</p>
+        <p class="text-left">コトバノートは小説やポエムの一節、または偉人の名言、街角の看板、あなたの心にのこったコトバを記録するためのサービスです。</p>
       </b-col>
     </b-row>
   </b-container>
