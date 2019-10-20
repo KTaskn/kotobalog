2f072aa2b765478b08ce2a3626b561dde7b45852
KTaskn <korai.kinjo@gmail.com>
2019-10-20 21:57:36 +0900

    ツイートボタンを追加

--- vue/src/components/SentenceCard.vue
+++ vue/src/components/SentenceCard.vue
@@ -1,6 +1,7 @@
 <template>
   <b-card class="sentence-card" :title="card.sentence" :sub-title="card.creator">
     <b-card-text v-if=show_text>{{ card.publisher }} {{ card.title }}</b-card-text>
+    <a class="twitter-share-button" v-bind:href="card.tweet_text">Tweet</a>
   </b-card>
 </template>
 
@@ -18,7 +19,8 @@ export default {
         sentence: '"' + this.sentence.sentence + '"',
         creator: this.sentence.creator,
         publisher: this.sentence.publisher,
-        title: this.sentence.title
+        title: this.sentence.title,
+        tweet_text: 'https://twitter.com/intent/tweet?text=' + this.sentence.sentence + ' - ' + this.sentence.creator + ' コトバノートで記録しました。'
       },
       show_text: false
     }
--- vue/src/global/index.js
+++ vue/src/global/index.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios'
-const APP_NAME = 'Kotobanote'
+const APP_NAME = 'コトバノート'
 const API_URL = process.env.API_URL
 
 export default {
