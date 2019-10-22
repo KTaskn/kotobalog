389ffb6c6b97078c4d9e4e938837ee658f6273b3
KTaskn <korai.kinjo@gmail.com>
2019-10-22 21:30:14 +0900

    ツイートボタンを修正

--- vue/src/components/SentenceCard.vue
+++ vue/src/components/SentenceCard.vue
@@ -1,7 +1,7 @@
 <template>
   <b-card class="sentence-card" :title="card.sentence" :sub-title="card.creator">
     <b-card-text v-if=show_text>{{ card.publisher }} {{ card.title }}</b-card-text>
-    <a class="twitter-share-button" v-bind:href="card.tweet_text">Tweet</a>
+    <b-button id="tweet-button" v-on:click="popUpTweetWindow" size="sm">ツイート</b-button>
   </b-card>
 </template>
 
@@ -19,23 +19,37 @@ export default {
         sentence: '"' + this.sentence.sentence + '"',
         creator: this.sentence.creator,
         publisher: this.sentence.publisher,
-        title: this.sentence.title,
-        tweet_text: encodeURI('https://twitter.com/intent/tweet?text=' + this.format_tweet_text(this.sentence.sentence, this.sentence.creator) + ' ＠コトバノートで記録しました。')
+        title: this.sentence.title
       },
       show_text: false
     }
   },
   methods: {
     format_tweet_text (text, creator) {
-      if (text.length > 30) {
-        return '' + text.substring(0, 30) + '...' + ' - ' + creator
+      if (text.length > 100) {
+        return '' + text.substring(0, 100) + '...' + ' - ' + creator
       } else {
         return '' + text + ' - ' + creator
       }
+    },
+    popUpTweetWindow () {
+      const url = encodeURI(
+        'https://twitter.com/intent/tweet?text=' +
+        this.format_tweet_text(this.sentence.sentence, this.sentence.creator) +
+        ' ＠コトバノートで記録しました。' +
+        ' https://kotobanote.jp/'
+      )
+      const option = 'status=1,width=818,height=400,top=100,left=100'
+      window.open(url, 'twitter', option)
     }
   }
 }
 </script>
 
 <style scoped>
+#tweet-button {
+  background-color: #00acee;
+  border-color: #00acee;
+  font: normal normal normal 11px/18px 'Helvetica Neue',Arial,sans-serif;
+}
 </style>
